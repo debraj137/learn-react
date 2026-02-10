a23dce9bc0cfe57bad066050628bb07fde388dc9
debrajdeb137 <debrajdeb137@gmail.com>
2026-02-10 22:10:33 +0530

    created error boundary

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,23 @@
+class ErrorBoundary extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {hasError: false};
+    }
+
+    static getDerivedStateFromError(error){
+        return {hasError: true};
+    }
+
+    componentDidCatch(error,info){
+        console.log("Error Caught: ",error,info);
+    }
+
+    render(){
+        if(this.state.hasError){
+            return <h1>Something went wrong</h1>
+        }
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
